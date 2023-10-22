ee553c71dc597a9735fc6dfabe07b0a655a697d5
Eric Shiflet <eshiflet@users.noreply.github.com>
2023-10-21 22:23:28 -0600

    new db

--- TDFapi/models/TDFModel.js
+++ TDFapi/models/TDFModel.js
@@ -2,42 +2,110 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var EntrySchema = new Schema({
-  riderfullname: {
-    type: String,
-    required: "Please enter a rider name"
-  },
-  ridernumber: {
+  RIDERNUMBER: {
     type: Number,
     required: "Please enter a rider bib number"
   },
-  riderid: {
+  RIDERNAME: {
+    type: String,
+    required: "Please enter a rider name"
+  },
+  TEAM: {
+      type: String,
+      required: "Please enter a team name"
+  },
+  RIDERID: {
     type: String,
      required: "Please enter a rider UUID"
   },
-  teamname: {
+  FINISH: {
     type: String,
-    required: "Please enter a team name"
+    required: "Please enter the overall race rank"
   },
-  s1: {
+  s1g: {
     type: Number,
     required: "Please enter a stage 1 ranking"
   },
-  s2: {
+  s2g: {
     type: Number,
     required: "Please enter a stage 2 ranking"
   },
-  s3: {
+  s3g: {
     type: Number,
     required: "Please enter a stage 3 ranking"
   },
-  s4: {
+  s4g: {
     type: Number,
     required: "Please enter a stage 4 ranking"
   },
-  s5: {
+  s5g: {
     type: Number,
     required: "Please enter a stage 5 ranking"
+  },
+  s6g: {
+    type: Number,
+    required: "Please enter a stage 6 ranking"
+  },
+  s7g: {
+    type: Number,
+    required: "Please enter a stage 7 ranking"
+  },
+  s8g: {
+    type: Number,
+    required: "Please enter a stage 8 ranking"
+  },
+  s9g: {
+    type: Number,
+    required: "Please enter a stage 9 ranking"
+  },
+  s10g: {
+    type: Number,
+    required: "Please enter a stage 10 ranking"
+  },
+  s11g: {
+    type: Number,
+    required: "Please enter a stage 11 ranking"
+  },
+  s12g: {
+    type: Number,
+    required: "Please enter a stage 12 ranking"
+  },
+  s13g: {
+    type: Number,
+    required: "Please enter a stage 13 ranking"
+  },
+  s14g: {
+    type: Number,
+    required: "Please enter a stage 14 ranking"
+  },
+  s15g: {
+    type: Number,
+    required: "Please enter a stage 15 ranking"
+  },
+  s16g: {
+    type: Number,
+    required: "Please enter a stage 16 ranking"
+  },
+  s17g: {
+    type: Number,
+    required: "Please enter a stage 17 ranking"
+  },
+  s18g: {
+    type: Number,
+    required: "Please enter a stage 18 ranking"
+  },
+  s19g: {
+    type: Number,
+    required: "Please enter a stage 19 ranking"
+  },
+  s20g: {
+    type: Number,
+    required: "Please enter a stage 20 ranking"
+  },
+  s21g: {
+    type: Number,
+    required: "Please enter a stage 21 ranking"
   }
 });
 
-module.exports = mongoose.model("Entry", EntrySchema, "TDF");
+module.exports = mongoose.model("Entry", EntrySchema, "2023tdfdata");
